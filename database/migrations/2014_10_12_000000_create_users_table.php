bf273d4f82985db9aa9a1c85e9e8af2d7108110a
zhouxiaoshuai3 <zhouxiaoshuai3@gmail.com>
2017-08-31 17:39:40 +0800

    本地化信息

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avatar');
+            $table->smallInteger('is_active')->default(0); //是否激活邮箱
+            $table->string('confirmation_token'); //激活字段
+            $table->integer('questions_count')->default(0);
+            $table->integer('answers_count')->default(0);
+            $table->integer('comments_count')->default(0);
+            $table->integer('favorites_count')->default(0);
+            $table->integer('likes_count')->default(0);
+            $table->integer('followers_count')->default(0); //关注
+            $table->integer('followings_count')->default(0); //被关注
+            $table->text('settings')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    knowledge-sharing!!!
+                    K~S
                 </div>
 
                 <div class="links">
